39699a06d694ce2d86f4ed83a818637402c701a4
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-10-26 11:06:17 +0330

    Add the script to use the __call static to handle dynamic static methods

--- helpers/url_helpers.php
+++ helpers/url_helpers.php
@@ -1,11 +1,11 @@
 <?php
 
-function site_url($url = '')
+function site_url(string $url = ''): string
 {
     return $_ENV['HOST_ADDR'] . $url;
 }
 
-function asset_url($url = '')
+function asset_url(string $url = ''): string
 {
     return site_url('public/' . $url);
 }
--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <!-- Front controller -->
 <?php
 
+use App\Utilities\Asset;
+
 include_once('./bootstrap/init.php');
+
 
 print_r(site_url());
 ?>
 <link href="<?php echo asset_url('css/styles.css'); ?>" rel="stylesheet" type="text/css">
 </link>
+<?php
+
+echo '<br />' . Asset::css('styles.css');
