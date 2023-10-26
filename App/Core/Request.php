30c196a8c83cb78b45d152f67244715db0d7ec15
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-10-26 09:40:18 +0330

    Add the PSR-4 folder structure and autoloading

---
+++ App/Core/Request.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Core;
+
+class Request
+{
+    function __construct()
+    {
+        echo self::class;
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,8 @@
 <!-- Front controller -->
 <?php
+include './vendor/autoload.php';
 echo "Here we go...";
+
+use App\Core\Request;
+
+new Request();
