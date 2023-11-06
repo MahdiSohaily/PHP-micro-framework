a82114520b02ec6c780e34f549d6d56ae042d127
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-11-06 14:24:33 +0330

    Add project base model contracts

--- App/Models/Contracts/BaseModel.php
+++ App/Models/Contracts/BaseModel.php
@@ -4,6 +4,33 @@ namespace App\Models\Contracts;
 
 
 
-abstract class BaseModel
+abstract class BaseModel implements CrudInterface
 {
+    protected $connection;
+    protected $table;
+    protected $primaryKey = "id";
+    protected $attribute = [];
+
+    protected function __construct($connection, $table, $primaryKey)
+    {
+    }
+
+
+    protected function getPrimaryKey()
+    {
+        return $this->primaryKey;
+    }
+
+    protected function setPrimaryKey($primaryKey)
+    {
+        $this->primaryKey = $primaryKey;
+    }
+
+    protected function getAttribute($key)
+    {
+        if (is_null($key) || !array_key_exists($key, $this->attribute)) {
+            return null;
+        }
+        return $this->attribute[$key];
+    }
 }
---
+++ App/Models/Contracts/MysqlBaseModel.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models\Contracts;
+
+
+class mysqlBaseModel extends BaseModel
+{
+}
