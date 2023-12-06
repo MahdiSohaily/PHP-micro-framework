00bd7ecf1e3f2e5214d376a2b5c8527e27cb1c50
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-12-06 12:52:41 +0330

    Add the project favicon

--- views/errors/404.php
+++ views/errors/404.php
@@ -4,8 +4,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?= asset_url('img/favicon.ico') ?>" type="image/x-icon">
     <title>Page not found</title>
-    <link rel="stylesheet" href="<?= asset_url('css/output.css')?>"> </link>
+    <link rel="stylesheet" href="<?= asset_url('css/output.css') ?>">
+    </link>
 </head>
 
 <body>
