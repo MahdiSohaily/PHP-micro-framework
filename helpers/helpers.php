8db70beef1f8ae8afa85df89fcad97c6618a7a95
MahdiSohaily <mahdi.sohaily4030@gmail.com>
2023-12-06 11:46:35 +0330

    Add environments variables

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -20,12 +20,3 @@ function view($path = '', $data = [])
     $fullPath = BASE_PATH . "views/$filePath.php";
     include_once $fullPath;
 }
-
-function strContains(string $str, string $needle = "", bool $case_sensitive = false): bool
-{
-    if ($case_sensitive) {
-        return strpos($str, $needle) !== false;
-    } else {
-        return stripos($str, $needle) !== false;
-    }
-}
